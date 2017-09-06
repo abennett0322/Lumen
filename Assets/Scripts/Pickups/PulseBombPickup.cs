9a6e53f3e72acce900f35e9cad1f37d8ac3beebb
BAH\587820 <bennett_andrew@bah.com>
2017-09-06 11:49:28 -0500

    Add Particle Effect
    Added particle effect to the pulse bomb pickups

--- Assets/Scripts/Pickups/PulseBombPickup.cs
+++ Assets/Scripts/Pickups/PulseBombPickup.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class PulseBombPickup : MonoBehaviour {
 
+	public ParticleSystem pickupParticles;
 
 	void OnTriggerEnter(Collider other){
 		if (other.gameObject.tag.Equals("Player")){
+			Destroy(Instantiate (pickupParticles.gameObject, gameObject.transform.position, Quaternion.LookRotation(Vector3.up)) as GameObject, pickupParticles.main.startLifetime.constant);
 			Destroy (gameObject);
 			PulseBombManager.pulseBombCount += 1;
 		}
