fab187f5fee3b97e271825db41ef8f5ac99d2b54
Andrew Bennett <abennett0322@gmail.com>
2017-11-29 13:02:43 -0600

    Add Xbox Live Service

--- Assets/Scripts/PauseGame.cs
+++ Assets/Scripts/PauseGame.cs
@@ -2,21 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PauseGame : MonoBehaviour {
 
 	public Canvas pauseMenu;
 	public Canvas gameHUD;
 	bool paused = false;
+	bool restart = false;
 
-	Button continueButton;
+	public Button continueButton;
+	public Button restartButton;
 
 	void Start () {
 		gameHUD.enabled = true;
 		pauseMenu.enabled = false;
 		Time.timeScale = 1;
 
-		continueButton = pauseMenu.GetComponentInChildren<Button> ();
+		continueButton.onClick.AddListener (Resume);
+		restartButton.onClick.AddListener (Restart);
 	}
 
 	void Update () {
@@ -25,8 +29,6 @@ public class PauseGame : MonoBehaviour {
 		if(Input.GetButtonDown("Pause")) {
 			Pause ();
 		}
-
-		continueButton.onClick.AddListener (Resume);
 	}
 
 	void Pause() {
@@ -36,6 +38,7 @@ public class PauseGame : MonoBehaviour {
 			gameHUD.enabled = false;
 			pauseMenu.enabled = true;
 			Time.timeScale = 0;
+
 		} else {
 			gameHUD.enabled = true;
 			pauseMenu.enabled = false;
@@ -45,9 +48,20 @@ public class PauseGame : MonoBehaviour {
 
 	void Resume() {
 		paused = false;
+		Debug.Log ("Resuming");
 
 		gameHUD.enabled = true;
 		pauseMenu.enabled = false;
 		Time.timeScale = 1;
+	}
+
+	void Restart() {
+		restart = true;
+
+		if (restart) {
+			Debug.Log ("Restarting");
+			restart = false;
+			SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex);
+		}
 	}
 }
